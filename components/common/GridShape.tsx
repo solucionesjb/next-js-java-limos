3352ced0b434f38dd0591c69b740b601809ea558
Josue Buitron Ortiz <josue.buitron+cemex@ext.cemex.com>
2026-03-03 14:57:48 -0600

    Sing in

--- components/common/GridShape.tsx
+++ components/common/GridShape.tsx
@@ -8,7 +8,7 @@ export default function GridShape() {
         <Image
           width={540}
           height={254}
-          src="/images/shape/grid-01.svg"
+          src="/public/images/shape/grid-01.svg"
           alt="grid"
         />
       </div>
